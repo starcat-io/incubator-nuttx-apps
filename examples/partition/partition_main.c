1bfae0793edb6ab345286998a2793899195cd456
Adam Feuer <adam@adamfeuer.com>
2020-03-05 22:23:30 -0800

    added partition table parsing example

---
+++ examples/partition/partition_main.c
@@ -0,0 +1,166 @@
+/****************************************************************************
+ * examples/partition/partition_main.c
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <debug.h>
+#include <errno.h>
+#include <poll.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+char *device_name = "/dev/sda";
+
+/****************************************************************************
+ * Private Function Prototypes
+ ****************************************************************************/
+static int parse_partition_prototypes(void);
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+struct partition
+{
+    unsigned char boot_flag;        /* 0 = Not active, 0x80 = Active */
+    unsigned char chs_begin[3];
+    unsigned char sys_type;         /* For example : 82 --> Linux swap, 83 --> Linux native partition, ... */
+    unsigned char chs_end[3];
+    unsigned char start_sector[4];
+    unsigned char nr_sector[4];
+};
+
+void string_in_hex(void *in_string, int in_string_size);
+void dump_partition(struct partition *part, int partition_number);
+
+void dump_partition(struct partition *part, int partition_number)
+{
+    printf("Partition %s%d\n", device_name, partition_number + 1);
+    printf("boot_flag = %02X\n", part->boot_flag);
+    printf("chs_begin = ");
+    string_in_hex(part->chs_begin, 3);
+    printf("sys_type = %02X\n", part->sys_type);
+    printf("chs_end = ");
+    string_in_hex(part->chs_end, 3);
+    printf("start_sector = ");
+    string_in_hex(part->start_sector, 4);
+    printf("nr_sector = ");
+    string_in_hex(part->nr_sector, 4);
+}
+
+void string_in_hex(void *in_string, int in_string_size)
+{
+    int i;
+    int k = 0;
+
+    for (i = 0; i < in_string_size; i++)
+    {
+        printf("%02x ", ((char *)in_string)[i]& 0xFF);
+        k = k + 1;
+        if (k == 16)
+        {
+            printf("\n");
+            k = 0;
+        }
+    }
+    printf("\n");
+}
+
+static int parse_partition_table(void)
+{
+    int /*gc = 0,*/ i = 1, nr = 0, pos = -1/*, nw = 0*/;
+    int fd = 0;
+    char  buf[512] ;
+    struct partition *sp;
+    int ret = 0;
+
+    printf("Ok ");
+
+    if ((fd = open(device_name, O_RDONLY | O_SYNC)) == -1)
+    {
+        perror("Open");
+        exit(1);
+    }
+    printf("fd is %d\n", fd);
+
+    pos = lseek (fd, 0, SEEK_CUR);
+
+    printf("Position of pointer is :%d\n", pos);
+    if ((nr = read(fd, buf, sizeof(buf))) == -1)
+    {
+        perror("Read");
+        exit(1);
+    }
+
+    ret = close(fd);
+    if (ret == -1)
+    {
+        perror("close");
+        exit(1);
+    }
+
+    /* Dump the MBR buffer, you can compare it on your system with the output of the command:
+     * hexdump -n 512 -C /dev/sda
+     */
+    string_in_hex(buf, 512);
+
+    printf("Size of buf = %d - and number of bytes read are %d\n", sizeof(buf), nr);
+    /*if ((nw = write(1, buf, 64)) == -1)
+    {
+            printf("Write: Error");
+            exit(1);
+    }
+
+    printf("\n\n%d bytes are just been written on stdout\nthis can also be printed\n", nw);
+    */
+    //printf("\n\t\t*************Partition Table****************\n\n");
+    printf("\n\t\t*************THE 4 MAIN PARTITIONS****************\n\n");
+
+    /* Dump main partitions (4 partitions) */
+    /* Note : the 4 partitions you are trying to dump are not necessarily existing! */
+    for (i = 0 ; i < 4 ; i++)
+    {
+        sp = (struct partition *)(buf + 446 + (16 * i));
+        //putchar(sp->boot_flag);
+        dump_partition(sp, i);
+    }
+
+    return 0;
+}
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * partition_main
+ ****************************************************************************/
+
+int main(int argc, FAR char *argv[])
+{
+  int ret;
+
+  ret = parse_partition_table();
+
+  return ret;
+}
